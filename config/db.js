16f50e7c4ac36a793d86f14ea6c464da1544d6dd
mohamednatsu <msalhnwr@gmail.com>
2025-08-08 16:07:46 +0300

    update the db

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ exports.initDB = async function () {
         await sql`CREATE TABLE IF NOT EXISTS transactions
                   (
                       id SERIAL PRIMARY KEY,
-                      user_id INTEGER NOT NULL,
+                      user_id VARCHAR(255) NOT NULL,
                       title VARCHAR(255) NOT NULL,
                       amount DECIMAL NOT NULL,
                     category VARCHAR(255) NOT NULL,
